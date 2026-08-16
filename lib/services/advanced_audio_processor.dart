3e0448ed1302aff233bad66f1ff8f3016a398a79
Sanskar <sanskarin@outlook.in>
2026-08-16 07:20:41 +0000

    style: apply canonical Dart formatting to lib/services/advanced_audio_processor.dart

--- lib/services/advanced_audio_processor.dart
+++ lib/services/advanced_audio_processor.dart
@@ -170,8 +170,7 @@ class AdvancedAudioProcessor {
       outputTitle: outputTitle,
       format: format,
       bitRate: bitRate,
-      audioFilter:
-          'acompressor=threshold=-18dB:ratio=3:attack=20:release=250:makeup=2dB',
+      audioFilter: 'acompressor=threshold=-18dB:ratio=3:attack=20:release=250:makeup=2dB',
     );
   }
 
