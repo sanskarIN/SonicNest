04217539b9866d9289facb7d5a52ef9506d059ba
Sanskar <sanskarin@outlook.in>
2026-08-16 13:13:44 +0530

    style: format bootstrap integrity regression

--- test/bootstrap_integrity_test.dart
+++ test/bootstrap_integrity_test.dart
@@ -25,6 +25,9 @@ void main() {
 
     expect(script, contains("'analysis_options.yaml'"));
     expect(script, contains('finally'));
-    expect(script, contains('Copy-Item $AnalysisOptionsBackup $AnalysisOptions -Force'));
+    expect(
+      script,
+      contains('Copy-Item $AnalysisOptionsBackup $AnalysisOptions -Force'),
+    );
   });
 }
