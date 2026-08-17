0ac2211312ac73afda8d0d12096638c4279e778d
Sanskar <sanskarin@outlook.in>
2026-08-17 17:59:57 +0530

    test: cover canonical Gumroad storefront URL

---
+++ test/gumroad_integration_test.dart
@@ -0,0 +1,18 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:sonic_nest/core/constants.dart';
+
+void main() {
+  test('Gumroad storefront URL is the canonical HTTPS destination', () {
+    expect(
+      AppConstants.gumroadStoreUrl,
+      'https://ramsandesh.gumroad.com',
+    );
+
+    final uri = Uri.parse(AppConstants.gumroadStoreUrl);
+    expect(uri.scheme, 'https');
+    expect(uri.host, 'ramsandesh.gumroad.com');
+    expect(uri.path, isEmpty);
+    expect(uri.query, isEmpty);
+    expect(uri.fragment, isEmpty);
+  });
+}
